2effbc7af2482e751d9b8c8f4ba776f18ddc5582
Imren <imrenrahbay@gmail.com>
2025-01-26 12:19:14 +0100

    Calculator program final

---
+++ Weather App Project/index.js
@@ -0,0 +1,13 @@
+
+
+const weatherForm=document.querySelector(".weatherForm");
+const cityInput= document.querySelector(".cityInput");
+const card =document.querySelector(".card");
+const cityDisplay=document.querySelector(".cityDisplay");
+const apiKey = "";
+
+
+weatherForm.addEventListener("submit", event =>{
+
+});
+
